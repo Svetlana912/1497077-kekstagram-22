ceeb3a9db977559300aeab539a8c7b6b09f58754
Sv Dj <sukachsvetlana921@gmail.com>
2021-02-09 22:01:40 +0300

    Задание два. Дубль два

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// версия с вебинара
 const randomInteger = (min, max) => {
   if (isNaN(min) || isNaN(max)) {
-  throw new Error('Некорректный тип аргументов.');
+    throw new Error('Некорректный тип аргументов.');
   }
   if (min < 0 || max < 0) {
-  throw new Error('Передано отрицательное значение.');
+    throw new Error('Передано отрицательное значение.');
   }
   if (min === max) return min;
   const coef = Math.random();
@@ -17,34 +16,9 @@ let result = 0;
 try {
   result = randomInteger(1, 15);
   } catch (error) {
-  alert(error);
+    alert(error);
   }
   
-// моя версия
-function randomInteger (min, max) {
-  if (min < 0 || max < 0 || isNaN(min) || isNaN(max)) {
-  throw new Error('Вы таки прислали фигню.');
-  }
-  if (min > max) {
-    [min, max] = [max, min];
-  }
 
-  return Math.floor(min + Math.random() * (max + 1 - min));
-} 
-
-try {
-  randomInteger(1, 15);
-  } catch (err) {
-  alert(err);
-  }
-
-// функция проверки длины сообщения
 const foo = (str, maxlength) => (typeof str === 'string' && 
   str.length <= maxlength) 
-     
-      
-  
-  
-
-  
-  ;  
