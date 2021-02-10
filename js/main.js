c56972b08b7a6ac581076ce778b422cc8d4bda12
Sv Dj <sukachsvetlana921@gmail.com>
2021-02-10 20:49:28 +0300

    Задача2 Дубль3

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-const randomInteger = (min, max) => {
+const getRandomNumber = (min, max) => {
   if (isNaN(min) || isNaN(max)) {
-    throw new Error('Некорректный тип аргументов.');
+    throw new Error("Некорректный тип аргументов.");
   }
   if (min < 0 || max < 0) {
-    throw new Error('Передано отрицательное значение.');
+    throw new Error("Передано отрицательное значение.");
   }
   if (min === max) return min;
   const coef = Math.random();
-  const x = (min < max) ? 1 : 0;
+  const x = min < max ? 1 : 0;
 
-  return Math.floor(coef *(Math.floor(max) - Math.floor(min) + x) + Math.floor(min)); 
-} 
- 
+  return Math.floor(
+    coef * (Math.floor(max) - Math.floor(min) + x) + Math.floor(min)
+  );
+};
+
 let result = 0;
 try {
-  result = randomInteger(1, 15);
-  } catch (error) {
-    alert(error);
-  }
-  
+  result = getRandomNumber(1, 15);
+} catch (error) {
+  alert(error);
+}
 
-const foo = (str, maxlength) => (typeof str === 'string' && 
-  str.length <= maxlength) 
+const checkStringLength = (str, maxlength) =>
+  typeof str === "string" && str.length <= maxlength;
